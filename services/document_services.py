58fc7602f5baa48bca1fcdc41288da1fd977a6f5
Andrea <lk.andrea@ymail.com>
2018-05-13 05:20:39 +0700

    Login modal

--- services/document_services.py
+++ services/document_services.py
@@ -12,3 +12,15 @@ class DocumentService():
             documents.append(Document(DocumentID=result[0], Title=result[1], Description=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=[5]))
 
         return documents
+
+    @staticmethod
+    @exception
+    def save_documents():
+        results = Document.objects.exec_sp_tolist(
+            'jh_Document_SaveDocuments')
+
+        documents = []
+        for result in results:
+            documents.append(Document(DocumentID=result[0], Title=result[1], Description=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=[5]))
+
+        return documents
