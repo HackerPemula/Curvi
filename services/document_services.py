833c57b78e5075de04904ed328c9364cf579f06f
Muhammad Ananda Ryanizar <anandar.ryanizar@gmail.com>
2018-05-13 06:50:09 +0700

    Doc rev

--- services/document_services.py
+++ services/document_services.py
@@ -9,7 +9,7 @@ class DocumentService():
 
         documents = []
         for result in results:
-            documents.append(Document(DocumentID=result[0], Title=result[1], Description=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=result[5]))
+            documents.append(Document(DocumentID=result[0], Title=result[1], Content=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=result[5]))
 
         return documents
 
@@ -17,22 +17,20 @@ class DocumentService():
     @exception
     def get_document_by_id(param):
         result = Document.objects.exec_sp_tosingle('jh_Document_GetDocumentsByID', param)
-        document = Document(DocumentID=result[0], Title=result[1], Description=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=result[5], CategoryID=result[6])
+        document = Document(DocumentID=result[0], Title=result[1], Content=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=result[5], CategoryID=result[6])
         return document
 
     @staticmethod
     @exception
     def update_document_category_by_id(param):
-        result = Document.objects.exec_sp('jh_Document_UpdateDocumentsCategoryByID', param)
+        result = Document.objects.exec_sp('jh_Document_UpdateDocumentCategoryByID', param)
         return result
 
     @staticmethod
     @exception
     def save_documents(param):
-        results = Document.objects.exec_sp_tolist('jh_Document_SaveDocuments', param)
+        result = Document.objects.exec_sp_tosingle('jh_Document_SaveDocuments', param)
 
-        documents = []
-        for result in results:
-            documents.append(Document(DocumentID=result[0], Title=result[1], Description=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=[5], CategoryID=result[6]))
+        documents = Document(DocumentID=result[0], Title=result[1], Content=result[2], RegistrantID=result[3], DocumentUrl=result[4], StatusRecord=[5], CategoryID=result[6])
 
         return documents
